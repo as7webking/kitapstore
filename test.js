9d75e8ad0532af56b75e6c6c33800c7e3ac4be25
Ahmed Seithalilov <43552101+as7webking@users.noreply.github.com>
2026-01-05 17:27:07 +0100

    Added frontend files and directories

---
+++ test.js
@@ -0,0 +1,18 @@
+//Test file
+import { MongoClient } from "mongodb";
+
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected successfully to MongoDB");
+  } catch (err) {
+    console.error("Connection error:", err);
+  } finally {
+    await client.close();
+  }
+}
+
+run();
